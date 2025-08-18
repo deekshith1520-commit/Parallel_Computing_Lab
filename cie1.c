3bc22ed6a353fbe7c5dd40638b16688056162296
Deekshith <deekshith1520@gmail.com>
2025-08-18 10:34:15 +0530

    second commit

---
+++ cie1.c
@@ -0,0 +1,13 @@
+// write an open mp program which launches a parrel region and it has each thread prints its id and  team size
+
+#include <stdio.h>
+#include <omp.h>
+int main(){
+#pragma omp parallel
+{
+    int thread_id = omp_get_thread_num();
+    int team_size = omp_get_num_threads();
+    printf("thread %d out of %d threads\n", thread_id, team_size);
+}
+ return 0;
+}
